ef81c9ca642fcea7d9ba95fff6046fa68bf61583
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-07-19 22:16:48 -0300

    Fix URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://desafio-carrinho-react.netlify.app',
-  // baseURL: 'http://localhost:3333',
+  baseURL: 'http://localhost:3333',
 });
